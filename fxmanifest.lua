b78289f4707358c0886a0749b4867bc375e0b4c8
Hassan Nazar <hassannazar72@gmail.com>
2025-06-22 15:48:49 +0500

    updated Fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 description 'Black Market Script'
 author 'PulseScripts'
-version '1.0.1'
+version '1.0.2'
 
 description 'Black Market Script by PulseScripts https://discord.gg/72Y7WKsP9M'
 
